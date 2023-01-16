5a0a458cf89a54ddfeabf75d5ae6736361849af7
Luke Dupont <lukesdupont@gmail.com>
2023-01-16 22:55:02 +0900

    added --help to pv.py

--- pv.py
+++ pv.py
@@ -6,9 +6,11 @@ i = 0
 
 fv = float(sys.argv[1])
 #print("Future Value: ", fv)
+if "-h" in sys.argv or "--help" in sys.argv or len(sys.argv) < 5:
+    print("Usage:")
+    print("python3 pv.py <future-value> -r <discount-rate> -p <period>")
+    exit()
 for arg in sys.argv:
-    if arg=="-h" or arg=="--help":
-        print("Help")
     if i>1:
         if arg=="-p" or arg=="--period":
             period = float(sys.argv[i+1])
